c7638fefa85f56c687144a95dda4ee6d5f8a8128
Jonathan <84401668+Jonathancastrosilva@users.noreply.github.com>
2021-09-01 13:54:43 -0300

    Update quadradomagico.js

--- quadradomagico.js
+++ quadradomagico.js
@@ -1,1 +1,26 @@
+const ordem = 3;
 
+document.addEventListener('DOMContentLoaded', () => {
+    insereTabela();
+});
+
+function insereTabela() {
+    const tabela = document.createElement('table');
+    tabela.id = 'quadradomagico'
+    document.body.append(tabela);
+    for (let i=0; i<ordem; i++){
+        const linha = document.createElement('tr');
+        tabela.append(linha);
+        for (let j=0; j<ordem; j++) {
+            const celula = document.createElement('td');
+            linha.append(celula);
+            celula.id = `lin${i}col${j}`;
+            insereInput(celula);
+        }
+    }
+}
+
+function insereInput(celula){
+    const input = document.createElement('input');
+    celula.append(input);
+}
