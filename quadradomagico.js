e5e0726973fc16ba6025f925864e4a54b6e01a05
Jonathan <84401668+Jonathancastrosilva@users.noreply.github.com>
2021-09-09 23:25:13 -0300

    Update quadradomagico.js

--- quadradomagico.js
+++ quadradomagico.js
@@ -1,24 +1,47 @@
-const ordem = 3;
-const matriz = Array(ordem)
-for (let i=0; i<matriz.length; i++) {
-    matriz[i] = Array(ordem);
-}
-const somaNumeros = 15;
+let ordem = 0;
+let matriz = [];
+let somaNumeros = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
-    insereTabela();
+    iniciaAe();
 });
+
+function iniciaAe() {
+    const div = document.createElement('div');
+    document.body.append(div);
+
+    const textoInicio = document.createElement('p');
+    textoInicio.classList.add('textoinicio');
+    textoInicio.innerText = "numero de colunas:"
+    div.append(textoInicio);
+
+    const input = document.createElement('input');
+    input.classList.add('ordem');
+    div.append(input)
+
+    input.addEventListener('change', () => {
+        ordem = parseInt(input.value);
+        matriz = Array(ordem)
+        for (let i=0; i<matriz.length; i++) {
+            matriz[i] = Array(ordem);
+        }
+        somaNumeros = (ordem + ordem**3)/2;
+        textoInicio.remove();
+        input.remove();
+        insereTabela();
+    });
+}
 
 function insereTabela() {
     const tabela = document.createElement('table');
-    tabela.id = 'quadradomagico'
+    tabela.id = 'quadradomagico';
     document.body.append(tabela);
-    for (let i=0; i<ordem; i++){
+    for (let i=0; i<ordem; i++) {
         const linha = document.createElement('tr');
         tabela.append(linha);
         for (let j=0; j<ordem; j++) {
             const celula = document.createElement('td');
-            linha.append(celula);
+            linha.append(celula)
             celula.id = `lin${i}col${j}`;
             insereInput(celula);
         }
@@ -27,7 +50,7 @@ function insereTabela() {
 
 function getLinhaColuna(celula) {
     const [linha,coluna] = celula.id.split('col');
-    return [linha.split('lin')[1], coluna];
+    return [linha.split('lin')[1],coluna];
 }
 
 function insereInput(celula) {
@@ -35,28 +58,53 @@ function insereInput(celula) {
     celula.append(input);
     input.addEventListener('change', () => {
         const valor = parseInt(input.value);
-        const [linha, coluna] = getLinhaColuna(celula);
-        matriz[linha][coluna] = valor
-        const quadradocompleto = verificamatriz();
-        if (quadradocompleto) {
+        const [linha,coluna] = getLinhaColuna(celula);
+        matriz[linha][coluna] = valor;
+        const quadradroCompleto = verificaMatriz();
+        if (quadradroCompleto) {
             document.querySelector('#quadradomagico').classList.add('vitoria');
-        } else {
-            document.querySelector('#quadradomagico').classList.remove('vitoria');
+            document.querySelectorAll('input').forEach(input => {
+                input.readOnly = true;
+            })
+            criaTextoParabens();
+            criaBotaoReinicia();
         }
     });
 }
 
-function verificamatriz() {
+function criaTextoParabens() {
+    const parabens = document.createElement('p');
+    parabens.innerText = "ir dnv?"
+    document.body.append(parabens);
+}
+
+function criaBotaoReinicia() {
+    const botaoReinicia = document.createElement('button');
+    botaoReinicia.innerText = "Reiniciar"
+    document.body.append(botaoReinicia);
+    botaoReinicia.addEventListener('click', () => {
+        const tabela = document.querySelector('#quadradomagico');
+        const parabens = document.querySelector('p');
+        tabela.remove();
+        parabens.remove();
+        botaoReinicia.remove();
+        iniciaAe();
+    })
+}
+
+
+
+function verificaMatriz() {
     const numerosRepetidos = verificaNumerosRepetidos();
     const numerosForaDosLimites = verificaNumerosForaDosLimites();
-    const todasSomaOK = verificaSomas ();
+    const todasSomaOK = verificaSomas();
     return !numerosRepetidos && !numerosForaDosLimites && todasSomaOK;
 }
 
 function verificaSomas() {
-    const diagonalPrincipalOK = verificaSomaDiagonalPrincipal ();
-    const diagonalSecundariaOK = verificaSomaDiagonalSecundaria ();
-    const todasLinhasOK = verificaSomaLinhas ();
+    const diagonalPrincipalOK = verificaSomaDiagonalPrincipal();
+    const diagonalSecundariaOK = verificaSomaDiagonalSecundaria();
+    const todasLinhasOK = verificaSomaLinhas();
     const todasColunasOK = verificaSomaColunas();
     return diagonalPrincipalOK && diagonalSecundariaOK && todasLinhasOK && todasColunasOK;
 }
@@ -66,14 +114,14 @@ function verificaSomaColunas() {
     for (let j=0; j<ordem; j++) {
         todasColunasOK &= verificaSomaColuna(j);
     }
-    return todasColunasOK
+    return todasColunasOK;
 }
 
 function verificaSomaColuna(j) {
     let soma = 0;
-    for(let i=0; i<ordem; i++) {
+    for (let i=0; i<ordem; i++) {
         if (matriz[i][j] == null) return false;
-        soma += matriz[i][j];
+        soma += matriz [i][j];
     }
     if (soma != somaNumeros) {
         for (let i=0; i<ordem; i++) {
@@ -93,14 +141,14 @@ function verificaSomaLinhas() {
     for (let i=0; i<ordem; i++) {
         todasLinhasOK &= verificaSomaLinha(i);
     }
-    return todasLinhasOK
+    return todasLinhasOK;
 }
 
 function verificaSomaLinha(i) {
     let soma = 0;
     for (let j=0; j<ordem; j++) {
         if (matriz[i][j] == null) return false;
-        soma += matriz[i][j];
+        soma += matriz [i][j];
     }
     if (soma != somaNumeros) {
         for (let j=0; j<ordem; j++) {
@@ -117,9 +165,9 @@ function verificaSomaLinha(i) {
 
 function verificaSomaDiagonalSecundaria() {
     let soma = 0;
-    for(let i=0; i<ordem; i++) {
+    for (let i=0; i<ordem; i++) {
         if (matriz[i][ordem-i-1] == null) return false;
-        soma += matriz[i][ordem-i-1];
+        soma += matriz [i][ordem-i-1];
     }
     if (soma != somaNumeros) {
         for (let i=0; i<ordem; i++) {
@@ -136,9 +184,9 @@ function verificaSomaDiagonalSecundaria() {
 
 function verificaSomaDiagonalPrincipal() {
     let soma = 0;
-    for(let i=0; i<ordem; i++) {
+    for (let i=0; i<ordem; i++) {
         if (matriz[i][i] == null) return false;
-        soma += matriz[i][i];
+        soma += matriz [i][i];
     }
     if (soma != somaNumeros) {
         for (let i=0; i<ordem; i++) {
@@ -157,17 +205,17 @@ function verificaNumerosForaDosLimites() {
     const minimo = 1;
     const maximo = ordem**2;
     let numerosForaDosLimites = false;
-    for (let i=0; i<ordem; i++){
+    for (let i=0; i<ordem; i++) {
         for (let j=0; j<ordem; j++){
             if (matriz[i][j] < minimo || matriz[i][j] > maximo) {
                 numerosForaDosLimites = true;
-                atribuiClasseCelula('foradoslimites', i ,j);
+                atribuiClasseCelula('foradoslimites', i, j);
             } else {
-                removeClasseCelula('foradoslimites', i ,j);
+                removeClasseCelula('foradoslimites', i, j);
             }
         }
     }
-    return numerosForaDosLimites
+    return numerosForaDosLimites;
 }
 
 function verificaNumerosRepetidos() {
@@ -179,19 +227,19 @@ function verificaNumerosRepetidos() {
             if (!isNaN(valor)) {
                 numeros[valor-1]++;
             }
-        }    
-    }       
+        }
+    }
     for (let i=0; i<ordem; i++) {
         for (let j=0; j<ordem; j++) {
-            const valor = matriz[i][j];
+            const valor = matriz [i][j];
             if (!isNaN(valor) && numeros[valor-1] > 1) {
                 numerosRepetidos = true;
-                atribuiClasseCelula('numerosrepetidos', i ,j);
+                atribuiClasseCelula('numerosrepetidos', i, j);
             } else {
-                removeClasseCelula('numerosrepetidos', i ,j);
+                removeClasseCelula('numerosrepetidos', i, j);
             }
         }
-    } 
+    }
     return numerosRepetidos;
 }
 
